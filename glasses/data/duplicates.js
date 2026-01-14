bb2db1687d7b6a59a73d726d45481c56cea12776
petar.andreev <petar.andreev@ysura.com>
2026-01-14 10:51:21 +0100

    a

---
+++ glasses/data/duplicates.js
@@ -0,0 +1,11 @@
+const glassesSweden = [
+    {
+        name: '1906 Reserva Especial',
+        type: 'Copa 330ml',
+        description: '',
+        brewery: 'Estrella Galicia',
+        country: 'Duplicates',
+        image: 'glasses/img/Spain/1906_Reserva_330ml.png',
+        price: '2.00'
+    },
+];
